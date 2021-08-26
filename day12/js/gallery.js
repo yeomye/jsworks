7c5527ee929923661c81531b62caf02682a9a563
yyz2999@naver.com <yyz2999@naver.com>
2021-08-26 13:22:12 +0900

    day12 소스파일

---
+++ day12/js/gallery.js
@@ -0,0 +1,14 @@
+//nextGallery()만들기
+var num = 1;    //전역변수
+
+function nextGallery(){
+    num++;
+    if(num>7){num = 1;}
+    document.getElementById('pic').src='./images/img' + num + '.jpg';
+}
+
+function prevGallery(){
+    num--;
+    if(num<1){num = 7;}
+    document.getElementById('pic').src='./images/img' + num + '.jpg';
+}
