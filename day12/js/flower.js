e14078b50c72eeb709eab108668286cb9ba48adc
yyz2999@naver.com <yyz2999@naver.com>
2021-08-27 13:18:46 +0900

    day12 소스파일

---
+++ day12/js/flower.js
@@ -0,0 +1,12 @@
+function showText(){    
+    //설명글 보이기
+    document.getElementById('desc').style.display='block';
+    //보기버튼 숨기기
+    document.getElementById('open').style.display='none';
+}
+function hideText(){
+    //설명글 숨기기
+    document.getElementById('desc').style.display='none';
+    //보기버튼 보이기
+    document.getElementById('open').style.display='block';
+}
